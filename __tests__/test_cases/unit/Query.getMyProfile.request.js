182d5e1a2fc2bb7926cf0e8691c706bf1b0df5ba
Igor Omelchenko <megapixel23@gmail.com>
2020-11-10 21:44:08 +0200

    Prettier formatter added.

--- __tests__/test_cases/unit/Query.getMyProfile.request.js
+++ __tests__/test_cases/unit/Query.getMyProfile.request.js
@@ -2,13 +2,15 @@ const path = require('path');
 const Chance = require('chance');
 const given = require('../../steps/given');
 const when = require('../../steps/when');
-const then = require('../../steps/then');
 
 const chance = new Chance();
 
 describe('Query.getMyProfile.request template', () => {
-  it('Should use username as \'id\'', () => {
-    const templatePath = path.resolve(__dirname, '../../../mapping-templates/Query.getMyProfile.request.vtl');
+  it("Should use username as 'id'", () => {
+    const templatePath = path.resolve(
+      __dirname,
+      '../../../mapping-templates/Query.getMyProfile.request.vtl',
+    );
     const username = chance.guid();
     const context = given.an_appsync_context({ username }, {});
     const result = when.we_invoke_an_appsync_template(templatePath, context);
@@ -16,11 +18,11 @@ describe('Query.getMyProfile.request template', () => {
     expect(result).toEqual({
       version: '2018-05-29',
       operation: 'GetItem',
-      key : {
+      key: {
         id: {
-          S: username
-        }
-      }
-    })
+          S: username,
+        },
+      },
+    });
   });
 });
