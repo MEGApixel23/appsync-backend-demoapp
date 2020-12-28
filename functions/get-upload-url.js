d2e6112c6da6bf5b3d7d30f01bb8f62d6aa00e1b
Igor Omelchenko <megapixel23@gmail.com>
2020-12-28 17:54:55 +0200

    get-upload-url query added.

---
+++ functions/get-upload-url.js
@@ -0,0 +1,35 @@
+const S3 = require('aws-sdk/clients/s3');
+const ulid = require('ulid');
+
+const s3 = new S3({ useAccelerateEndpoint: true });
+
+const { BUCKET_NAME } = process.env;
+
+module.exports.handler = (event) => {
+  const id = ulid.ulid();
+  let key = `${event.identity.username}/${id}`;
+  const extension = event.arguments.extension;
+
+  if (extension) {
+    if (extension.startsWith('.')) {
+      key += extension;
+    } else {
+      key = `.${extension}`;
+    }
+  }
+
+  const contentType = event.arguments.contentType || 'image/jpeg';
+
+  if (!contentType.startsWith('image')) {
+    throw new Error('contentType should be an image');
+  }
+
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: key,
+    ACL: 'public-read',
+    ContentType: contentType,
+  };
+
+  return s3.getSignedUrl('putObject', params);
+};
